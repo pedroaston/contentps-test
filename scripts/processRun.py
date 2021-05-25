a45423c005cee99e2314ae7ec4174ec3ca40b779
pedroaston <pedroaston@gmail.com>
2021-05-25 05:36:41 -0700

    metrics parameters finished

--- scripts/processRun.py
+++ scripts/processRun.py
@@ -1,6 +1,10 @@
 import os
 import json
+import sys
 
+########################################################
+## Interprets a line (metric) from a results.out file ##
+########################################################
 def process_results_line(l):
     l = json.loads(l)
     name = l["name"]
@@ -32,28 +36,39 @@ def digested_results(res, test):
     
     summary = {}
     final_data = {}
-    runs = len(res)/len(test)
+    num_metrics = 0
     
     for measure in test:
         summary[measure] = 0
         final_data[measure + "/max"] = 0
     for item in res:
         if item["name"] in test:
+            num_metrics += 1
             summary[item["name"]] += item["value"]
             if item["value"] > final_data[item["name"] + "/max"]:
                 final_data[item["name"] + "/max"] = item["value"]
 
+    peer_runs = num_metrics/len(test)
     for sums in summary:
-        final_data[sums + "/mean"] = summary[sums]/runs
+        final_data[sums + "/mean"] = summary[sums]/peer_runs
 
+    final_data["runs"] = peer_runs/18
     return final_data
 
 ##################
 ## Main Program ##
 ##################
-metrics = {"fast":['Avg time to sub - FastDelivery', 'Events received - FastDelivery', 'Avg latency of events - FastDelivery',
-     'Avg time to sub - FastDelivery', 'CPU used - FastDelivery', 'Memory used - FastDelivery']}
+metrics = {"fast": ['Avg time to sub - FastDelivery', 'Avg latency of events - FastDelivery','Avg time to sub - FastDelivery', 
+'CPU used - FastDelivery', 'Memory used - FastDelivery'],
+ "normal_scout": ['Avg event latency - ScoutSubs normal', 'Avg time to sub - ScoutSubs normal',
+'CPU used - ScoutSubs normal', 'Memory used - ScoutSubs normal'],
+ "normal_subBurst": ['Avg event latency - ScoutSubs subBurst', 'Avg time to sub - ScoutSubs subBurst', 
+'CPU used - ScoutSubs subBurst', 'Memory used - ScoutSubs subBurst'],
+ "normal_eventBurst": ['Avg event latency - ScoutSubs eventBurst', 'Avg time to sub - ScoutSubs eventBurst',
+'CPU used - ScoutSubs eventBurst', 'Memory used - ScoutSubs eventBurst']}
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 agg, testcases = aggregate_results(dir_path + "/../../../data/outputs/local_docker/contentps-test/")
 final_res = digested_results(agg, metrics["fast"])
-print(final_res)
+for i in final_res:
+    print(i + " >> {0}".format(final_res[i]))
